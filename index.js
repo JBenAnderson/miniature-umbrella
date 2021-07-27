0ccb8bc06f5100b33a2fefe02bb845a2bb3c0745
J Ben Anderson <revovisions@gmail.com>
2021-07-27 15:17:06 -0400

    added object database within index.js called 'contacts'

--- index.js
+++ index.js
@@ -3,6 +3,35 @@ var { graphqlHTTP } = require('express-graphql');
 var { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
+var contacts = [
+    {
+        name: "peter parker",
+        age: 21,
+        email: "peter@mit.edu",
+        courses: [
+            {number: "1.00", name: "engr comp"},
+            {number: "3.00", name: "intro bio"}
+        ]
+    },
+    {
+        name: "diana prince",
+        age: 704,
+        email: "diana@mit.edu",
+        courses: [
+            {number: "2.00", name: "intro arch"},
+            {number: "1.00", name: "intro chem"}
+        ]
+    },
+    {
+        name: "bruce wayne",
+        age: 41,
+        email: "bruce@mit.edu",
+        courses: [
+            {number: "2.00", name: "intro ME"},
+            {number: "3.00", name: "intro MS"}
+        ]
+    }
+]
 var schema = buildSchema(`
   type Query {
     hello: String
